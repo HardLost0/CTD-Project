bcf142cbfcfbfe55b280258fa8c54dfc525eeb32
unknown <alexchan1233@gmail.com>
2025-07-01 01:18:24 -0500

    working code

---
+++ public/script.js
@@ -0,0 +1,38 @@
+const api = 'dbe139db718e460896b04127250107';
+
+document.querySelector('button').addEventListener('click', async () => {
+  const city = document.getElementById('cityInput').value.trim();
+  
+  if (!city) {
+    alert("Please enter a city name.");
+    return;
+  }
+
+  try {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=${api}&q=${(city)}`
+    );
+    const data = await response.json();
+
+    displayWeather(data);
+  } catch (error) {
+    showError(error.message);
+  }
+});
+
+function displayWeather(data) {
+  document.getElementById('weatherResults').innerHTML = `
+    <div class="weather-card">
+      <h2>${data.location.name}, ${data.location.country}</h2>
+      <p>${data.current.temp_f}°F</p>
+    </div>
+  `;
+}
+
+function showError(message) {
+  document.getElementById('weatherResults').innerHTML = `
+    <div class="weather-card error">
+      <p>Please Enter a City</p>
+    </div>
+  `;
+}
